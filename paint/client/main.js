193d7c9876f942aff6ecec712ad87e3831999fca
Miran <miran.loncaric@gmail.com>
2013-12-10 17:01:42 +0100

    Removed iframe check

--- paint/client/main.js
+++ paint/client/main.js
@@ -8,9 +8,9 @@ Teleport.templateName = "app";
 
 Teleport.readyCallback = function()
 {
-	if (window.top != window) {
+	/*if (window.top != window) {
       document.getElementById("header").style.display = "none";
-  }
+  }*/
   canvas = document.getElementById("drawCanvas");
   index = 0;
   colors = ["#828b20", "#b0ac31", "#cbc53d", "#fad779", "#f9e4ad", "#faf2db", "#563512", "#9b4a0b", "#d36600", "#fe8a00", "#f9a71f"];
